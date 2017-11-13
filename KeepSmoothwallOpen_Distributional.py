20ddf60a6bceb891a005e209db7b6d1ab6bcad9a
empireday <33628800+empireday@users.noreply.github.com>
2017-11-13 14:21:29 +0000

    main script

---
+++ KeepSmoothwallOpen_Distributional.py
@@ -0,0 +1,51 @@
+import requests #i dont use all request functions but im too lazy to change this for compression formatting.
+import time
+import getpass
+#fuck smoothwall, as of writing this they implemented a feature that logged you outta the internet with a smoothwall webpage every 10 mins
+#this program is for resending the TOTALLY UNENCRYPTED HTTP POST
+
+#FUCK SMOOTHWALL
+
+#this could be less ghetto but whatever lol
+user = input("Put in your username my dude, this shits unencoded:   ")
+print("thanks fam")
+
+passvar = getpass.getpass('Alright dude now for the password- Ive set it so the characters are invisible so dont freak out:   ')
+
+
+#YO I THOUGHT I NEEDED THIS SECTION WITH THE COOKIE BUT APPARENTLY THATS BULLSHIT LOL,
+#kept for fun archive purposes.
+'''
+print ("Now pass the computer to mr. deadfox the hackerman so he can sort out the cookie. ")
+time.sleep(5)
+print("Alternatively if you have a copy of securecookie already on your computer, please wait 5 seconds and input the value inside the text file.")
+time.sleep(5)
+var_cookie = input("ok dude last thing here is the only thing we need to get this going:     ")
+print("would you like to save this cookie? Y/N  ")
+save = input("Y/N   ")
+if save == ("Y", "y"):
+    f = open('securecookie.txt', 'w')
+    f.write(var_cookie)
+    '''
+
+#this sends the post
+loop = 1
+while True:
+    url = 'http://smoothwall.colchester.local'
+    requests.post("http://smoothwall.colchester.local/iclogin", data={'USERNAME': user, 'PASSWORD': passvar, 'ACTION': "Login"})
+    print("FUCK THE LEFT, TAXATION IS THEFT.")
+    time.sleep(15)
+# 1:30 minute wait between requests please.
+# Should be obvious as to why constant requests is bad
+
+    print("I am the world snake, Jourmangand.")
+    time.sleep(17)
+
+
+    print("Fuck smoothwall, the real villains making us fuck with the system. ")
+    time.sleep(15)
+    print("D E A D  F O X")
+    time.sleep(40)
+
+
+#, cookies={"SecureLogin": var_cookie}
