b90e62db3bfa44bbe8eba2edb1e0d7fa225d834c
empireday <33628800+empireday@users.noreply.github.com>
2018-01-16 08:48:39 +0000

    Update KeepSmoothwallOpen_Distributional.py

--- KeepSmoothwallOpen_Distributional.py
+++ KeepSmoothwallOpen_Distributional.py
@@ -4,7 +4,7 @@ import getpass
 #f*** smoothwall, as of writing this they implemented a feature that logged you outta the internet with a smoothwall webpage every 10 mins
 #this program is for resending the TOTALLY UNENCRYPTED HTTP POST
 
-#F*** SMOOTHWALL
+
 
 #this could be less ghetto but whatever lol
 user = input("Put in your username my dude:   ")
@@ -25,18 +25,4 @@ while True:
 # Should be obvious as to why constant requests is bad
 # OG post message was "F*** THE LEFT, TAXATION IS THEFT" because memes
 
--#YO I THOUGHT I NEEDED THIS SECTION WITH THE COOKIE BUT APPARENTLY THATS BULLS*** LOL,
--#kept for fun archive purposes.
--'''
--print ("Now pass the computer to the hackerman so he can sort out the cookie. ")
--time.sleep(5)
--print("Alternatively if you have a copy of securecookie already on your computer, please wait 5 seconds and input the value inside the text file.")
--time.sleep(5)
--var_cookie = input("ok dude last thing here is the only thing we need to get this going:     ")
--print("would you like to save this cookie? Y/N  ")
--save = input("Y/N   ")
--if save == ("Y", "y"):
--    f = open('securecookie.txt', 'w')
--    f.write(var_cookie)
--    '''
--
+
